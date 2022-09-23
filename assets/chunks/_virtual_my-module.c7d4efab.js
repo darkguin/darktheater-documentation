633e548ac555594df1bbb036fe16936fd091e291
ndt080 <ndt080@users.noreply.github.com>
2022-09-23 18:56:29 +0000

    deploy: 2dd150be0f368448812b5f5808c9d94fae90d0f6

---
+++ assets/chunks/_virtual_my-module.c7d4efab.js
@@ -0,0 +1,58 @@
+const a={version:"2.3.9",fields:["b","a"],fieldVectors:[["b/0.0",[]],["a/0.0",[0,1.872,1,1.603]],["b/1.0",[0,2.173,1,1.86,2,.757,3,.934,4,.675,5,3.723,6,2.646,7,2.646]],["a/1.0",[]],["b/2.0",[]],["a/2.0",[1,1.603,8,1.402]],["b/3.0",[2,.978,3,1.207,4,.62,9,1.862,10,1.661]],["a/3.0",[]],["b/4.0",[2,.978,3,1.207,4,.62,9,1.862,10,1.661]],["a/4.0",[]],["b/5.0",[2,.875,3,1.08,4,.555,11,1.334,12,.786,13,1.08,14,3.061]],["a/5.0",[]],["b/5.1",[4,.773,15,1.266,16,3.23,17,1.759,18,2.652]],["a/5.1",[12,.463,13,.637,15,.707]],["b/6.0",[2,.875,3,1.08,4,.555,11,1.334,12,.786,13,1.08,19,2.513]],["a/6.0",[]],["b/6.1",[17,2.108,19,3.177,20,3.871]],["a/6.1",[12,.586,13,.805]],["b/6.2",[4,.48,11,1.622,12,.956,15,1.037,21,1.627,22,1.627,23,2.173,24,1.86]],["a/6.2",[25,2.178]],["b/7.0",[2,.793,3,.978,4,.697,11,1.208,12,.712,13,.978,21,1.704,22,1.704]],["a/7.0",[]],["b/7.1",[8,2.38,17,2.108,26,3.871]],["a/7.1",[4,.231,12,.327,13,.449,21,.783,22,.783]],["b/7.2",[17,1.862,27,2.807,28,2.807,29,3.419,30,3.419]],["a/7.2",[12,.383,13,.527,27,1.226,28,1.226]],["b/7.3",[11,1.582,12,.932,15,1.423,24,2.552]],["a/7.3",[25,2.178]],["b/8.0",[2,.978,3,1.207,4,.62,9,1.862,10,1.661]],["a/8.0",[]],["b/8.1",[31,3.061,32,3.061,33,3.061,34,3.061,35,3.061,36,3.061,37,3.061]],["a/8.1",[38,2.281,39,2.281]],["b/9.0",[2,.694,3,.857,4,.44,8,1.492,11,1.058,12,.623,13,.857,15,1.372,40,1.992,41,2.427,42,2.427]],["a/9.0",[]],["b/9.1",[15,1.34,17,1.862,40,2.807,43,3.419,44,3.419]],["a/9.1",[12,.586,13,.805]],["b/9.2",[4,.48,11,1.622,12,.956,15,1.037,21,1.627,22,1.627,23,2.173,24,1.86]],["a/9.2",[25,2.178]],["b/10.0",[]],["a/10.0",[9,.983,10,.877,45,1.482]],["b/11.0",[2,.832,3,1.027,4,.528,9,1.584,10,1.414,46,2.045,47,2.795]],["a/11.0",[]],["b/12.0",[]],["a/12.0",[47,1.603,48,2.281]],["b/13.0",[2,.217,4,.333,8,.833,10,.369,15,.298,45,1.112,46,1.567,47,.952,49,1.355,50,.759,51,.759,52,.623,53,2.229,54,.759,55,.759,56,1.355,57,.759,58,.759,59,.759,60,.759,61,.759,62,.759,63,1.834,64,.759,65,1.834,66,.623,67,1.355,68,1.355,69,1.355,70,1.355,71,.623,72,1.834,73,.759,74,1.834,75,1.355,76,.759,77,.759,78,.759,79,.759]],["a/13.0",[]],["b/14.0",[]],["a/14.0",[66,2.543]],["b/15.0",[2,.464,4,.294,18,1.331,46,1.14,52,1.331,71,1.331,80,1.621,81,2.577,82,2.577,83,3.653,84,2.577,85,1.621,86,1.621,87,1.621,88,1.621,89,1.621,90,1.621,91,1.621]],["a/15.0",[]]],invertedIndex:[["",{_index:4,b:{"1.0":{},"3.0":{},"4.0":{},"5.0":{},"5.1":{},"6.0":{},"6.2":{},"7.0":{},"8.0":{},"9.0":{},"9.2":{},"11.0":{},"13.0":{},"15.0":{}},a:{"7.1":{}}}],["access",{_index:75,b:{"13.0":{}},a:{}}],["action",{_index:62,b:{"13.0":{}},a:{}}],["allowfullscreen></ifram",{_index:37,b:{"8.1":{}},a:{}}],["alt",{_index:67,b:{"13.0":{}},a:{}}],["api",{_index:1,b:{"1.0":{}},a:{"0.0":{},"2.0":{}}}],["auth",{_index:0,b:{"1.0":{}},a:{"0.0":{}}}],["author",{_index:14,b:{"5.0":{}},a:{}}],["backend",{_index:45,b:{"13.0":{}},a:{"10.0":{}}}],["below",{_index:89,b:{"15.0":{}},a:{}}],["board",{_index:39,b:{},a:{"8.1":{}}}],["brand",{_index:64,b:{"13.0":{}},a:{}}],["chosen",{_index:88,b:{"15.0":{}},a:{}}],["collect",{_index:42,b:{"9.0":{}},a:{}}],["comment",{_index:19,b:{"6.0":{},"6.1":{}},a:{}}],["content",{_index:8,b:{"7.1":{},"9.0":{},"13.0":{}},a:{"2.0":{}}}],["continu",{_index:27,b:{"7.2":{}},a:{"7.2":{}}}],["cool",{_index:87,b:{"15.0":{}},a:{}}],["darktheat",{_index:52,b:{"13.0":{},"15.0":{}},a:{}}],["databas",{_index:11,b:{"5.0":{},"6.0":{},"6.2":{},"7.0":{},"7.3":{},"9.0":{},"9.2":{}},a:{}}],["detail",{_index:74,b:{"13.0":{}},a:{}}],["develop",{_index:18,b:{"5.1":{},"15.0":{}},a:{}}],["doc",{_index:3,b:{"1.0":{},"3.0":{},"4.0":{},"5.0":{},"6.0":{},"7.0":{},"8.0":{},"9.0":{},"11.0":{}},a:{}}],["document",{_index:61,b:{"13.0":{}},a:{}}],["featur",{_index:71,b:{"13.0":{},"15.0":{}},a:{}}],['frameborder="0',{_index:35,b:{"8.1":{}},a:{}}],["friendli",{_index:76,b:{"13.0":{}},a:{}}],["frontend",{_index:47,b:{"11.0":{},"13.0":{}},a:{"12.0":{}}}],["gener",{_index:59,b:{"13.0":{}},a:{}}],["get",{_index:9,b:{"3.0":{},"4.0":{},"8.0":{},"11.0":{}},a:{"10.0":{}}}],["github",{_index:69,b:{"13.0":{}},a:{}}],["guid",{_index:86,b:{"15.0":{}},a:{}}],['height="432',{_index:33,b:{"8.1":{}},a:{}}],["hero",{_index:50,b:{"13.0":{}},a:{}}],["home",{_index:49,b:{"13.0":{}},a:{}}],["https://github.com/darkguin/darktheat",{_index:70,b:{"13.0":{}},a:{}}],["icon",{_index:72,b:{"13.0":{}},a:{}}],['id="auth_api',{_index:6,b:{"1.0":{}},a:{}}],["ifram",{_index:31,b:{"8.1":{}},a:{}}],["index",{_index:48,b:{},a:{"12.0":{}}}],["interfac",{_index:77,b:{"13.0":{}},a:{}}],["introduct",{_index:66,b:{"13.0":{}},a:{"14.0":{}}}],["layout",{_index:2,b:{"1.0":{},"3.0":{},"4.0":{},"5.0":{},"6.0":{},"7.0":{},"8.0":{},"9.0":{},"11.0":{},"13.0":{},"15.0":{}},a:{}}],["lead",{_index:85,b:{"15.0":{}},a:{}}],["link",{_index:65,b:{"13.0":{}},a:{}}],["list",{_index:40,b:{"9.0":{},"9.1":{}},a:{}}],["lists.png){data",{_index:44,b:{"9.1":{}},a:{}}],['members="memb',{_index:91,b:{"15.0":{}},a:{}}],["mileston",{_index:55,b:{"13.0":{}},a:{}}],["miro",{_index:38,b:{},a:{"8.1":{}}}],["model](/assets/img/backend/db/comments.png){data",{_index:20,b:{"6.1":{}},a:{}}],["model](/assets/img/backend/db/content.png){data",{_index:26,b:{"7.1":{}},a:{}}],["model](/assets/img/backend/db/continu",{_index:29,b:{"7.2":{}},a:{}}],["model](/assets/img/backend/db/us",{_index:43,b:{"9.1":{}},a:{}}],["model](/assets/img/backend/db/user.png){data",{_index:16,b:{"5.1":{}},a:{}}],["movi",{_index:21,b:{"6.2":{},"7.0":{},"9.2":{}},a:{"7.1":{}}}],["name",{_index:51,b:{"13.0":{}},a:{}}],["new",{_index:54,b:{"13.0":{}},a:{}}],["page",{_index:80,b:{"15.0":{}},a:{}}],["project",{_index:60,b:{"13.0":{}},a:{}}],["quick",{_index:73,b:{"13.0":{}},a:{}}],["refer",{_index:25,b:{},a:{"6.2":{},"7.3":{},"9.2":{}}}],["reliabl",{_index:79,b:{"13.0":{}},a:{}}],["schema",{_index:13,b:{"5.0":{},"6.0":{},"7.0":{},"9.0":{}},a:{"5.1":{},"6.1":{},"7.1":{},"7.2":{},"9.1":{}}}],["schemas](/backend/database/auth",{_index:24,b:{"6.2":{},"7.3":{},"9.2":{}},a:{}}],["schemas](/backend/database/cont",{_index:23,b:{"6.2":{},"9.2":{}},a:{}}],['scrolling="no',{_index:36,b:{"8.1":{}},a:{}}],["serial",{_index:22,b:{"6.2":{},"7.0":{},"9.2":{}},a:{"7.1":{}}}],["servic",{_index:57,b:{"13.0":{}},a:{}}],['src="https://miro.com/app/embed/uxjvpuhriry=/?pres=1&frameid=3458764534113588293&embedid=205706552703',{_index:34,b:{"8.1":{}},a:{}}],["stabl",{_index:78,b:{"13.0":{}},a:{}}],["start",{_index:10,b:{"3.0":{},"4.0":{},"8.0":{},"11.0":{},"13.0":{}},a:{"10.0":{}}}],["stream",{_index:56,b:{"13.0":{}},a:{}}],["swagger",{_index:5,b:{"1.0":{}},a:{}}],["tabl",{_index:12,b:{"5.0":{},"6.0":{},"6.2":{},"7.0":{},"7.3":{},"9.0":{},"9.2":{}},a:{"5.1":{},"6.1":{},"7.1":{},"7.2":{},"9.1":{}}}],["taglin",{_index:58,b:{"13.0":{}},a:{}}],["team",{_index:84,b:{"15.0":{}},a:{}}],["templat",{_index:83,b:{"15.0":{}},a:{}}],["text",{_index:53,b:{"13.0":{}},a:{}}],["theme",{_index:63,b:{"13.0":{}},a:{}}],["titl",{_index:46,b:{"11.0":{},"13.0":{},"15.0":{}},a:{}}],["ucc",{_index:41,b:{"9.0":{}},a:{}}],['url="https://api.darktheater.net/auth/doc',{_index:7,b:{"1.0":{}},a:{}}],["user",{_index:15,b:{"5.1":{},"6.2":{},"7.3":{},"9.0":{},"9.1":{},"9.2":{},"13.0":{}},a:{"5.1":{}}}],["view",{_index:68,b:{"13.0":{}},a:{}}],["vpteammemb",{_index:90,b:{"15.0":{}},a:{}}],["vpteampag",{_index:81,b:{"15.0":{}},a:{}}],["vpteampagetitl",{_index:82,b:{"15.0":{}},a:{}}],["watch",{_index:28,b:{"7.2":{}},a:{"7.2":{}}}],["watch.png){data",{_index:30,b:{"7.2":{}},a:{}}],['width="768',{_index:32,b:{"8.1":{}},a:{}}],["zoomabl",{_index:17,b:{"5.1":{},"6.1":{},"7.1":{},"7.2":{},"9.1":{}},a:{}}]],pipeline:["stemmer"]},e={"0.0":{t:" auth api",p:"",l:"backend/api/auth-api/index.html",a:" auth api"},"1.0":{t:"---",p:`layout: doc
+---
+
+# Auth API. Swagger
+
+<Swagger id="auth_api" u ...`,l:"backend/api/auth-api/open-api.html",a:"---"},"2.0":{t:" content api",p:"",l:"backend/api/content-api/index.html",a:" content api"},"3.0":{t:"---",p:`layout: doc
+---
+
+# Getting started`,l:"backend/api/content-api/open-api.html",a:"---"},"4.0":{t:"---",p:`layout: doc
+---
+
+# Getting started`,l:"backend/api/index.html",a:"---"},"5.0":{t:"---",p:`layout: doc
+---
+
+# Database table schemas. Authorization
+`,l:"backend/database/auth.html",a:"---"},"5.1":{t:"user table schema",p:`
+<!--- 
+![user model](/assets/img/backend/db/user.png){data-zo ...`,l:"backend/database/auth.html#user table schema",a:"user table schema"},"6.0":{t:"---",p:`layout: doc
+---
+
+# Database table schemas. Comments
+`,l:"backend/database/comments.html",a:"---"},"6.1":{t:"table schema",p:`
+![comments model](/assets/img/backend/db/comments.png){data-z ...`,l:"backend/database/comments.html#table schema",a:"table schema"},"6.2":{t:"references",p:`
+- [Movie & Serial Database Table Schemas](/backend/database/c ...`,l:"backend/database/comments.html#references",a:"references"},"7.0":{t:"---",p:`layout: doc
+---
+
+# Database table schemas. Movies & Serials
+
+`,l:"backend/database/content.html",a:"---"},"7.1":{t:"movie & serial table schema",p:"![content model](/assets/img/backend/db/content.png){data-zoom ...",l:"backend/database/content.html#movie & serial table schema",a:"movie & serial table schema"},"7.2":{t:"continue watch table schema",p:"![continue-watch model](/assets/img/backend/db/continue-watch. ...",l:"backend/database/content.html#continue watch table schema",a:"continue watch table schema"},"7.3":{t:"references",p:"- [User Database Table Schemas](/backend/database/auth)",l:"backend/database/content.html#references",a:"references"},"8.0":{t:"---",p:`layout: doc
+---
+
+# Getting started
+`,l:"backend/database/index.html",a:"---"},"8.1":{t:"miro board",p:`<iframe 
+    width="768" 
+    height="432" 
+    src="https://m ...`,l:"backend/database/index.html#miro board",a:"miro board"},"9.0":{t:"---",p:`layout: doc
+---
+
+# Database table schemas. User Lists (UCC - U ...`,l:"backend/database/user-lists.html",a:"---"},"9.1":{t:"table schema",p:`
+![user-lists model](/assets/img/backend/db/user-lists.png){da ...`,l:"backend/database/user-lists.html#table schema",a:"table schema"},"9.2":{t:"references",p:`
+- [Movie & Serial Database Table Schemas](/backend/database/c ...`,l:"backend/database/user-lists.html#references",a:"references"},"10.0":{t:" backend getting-started",p:"",l:"backend/index.html",a:" backend getting-started"},"11.0":{t:"---",p:`title: Frontend
+layout: doc
+---
+
+# Frontend getting-started`,l:"frontend/getting-started.html",a:"---"},"12.0":{t:" frontend index",p:"",l:"frontend/index.html",a:" frontend index"},"13.0":{t:"---",p:`title: Home
+layout: home
+
+hero:
+  name: darktheater
+  text: A  ...`,l:"index.html",a:"---"},"14.0":{t:" introduction",p:"",l:"introduction.html",a:" introduction"},"15.0":{t:"---",p:`layout: page
+---
+
+
+
+
+<VPTeamPage>
+  <VPTeamPageTitle>
+    <tem ...`,l:"team.html",a:"---"}},n={LUNR_DATA:a,PREVIEW_LOOKUP:e};export{n as default};
